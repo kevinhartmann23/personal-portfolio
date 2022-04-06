7fb1dc78500e17236efefcf09ebbc06ce3a0ba3c
Kevin Hartmann <kevinhartmann23@gmail.com>
2022-04-06 11:33:10 -0600

    Add poster

---
+++ src/Components/App/App.tsx
@@ -0,0 +1,12 @@
+import landingPhoto from '../../Assets/kev_dev.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <img className="poster" src={landingPhoto} alt="Kevin Hartmann Intro" />
+    </div>
+  )
+}
+
+export default App
